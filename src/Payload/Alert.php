42852b34778acf66fc53825785ac441bb6b570f2
Arthur Edamov <edamov@gmail.com>
2019-04-15 08:12:20 +0300

    Fix jsonSerialize for Alert

--- src/Payload/Alert.php
+++ src/Payload/Alert.php
@@ -341,10 +341,6 @@ class Alert implements \JsonSerializable
             $alert[self::ALERT_BODY_KEY] = $this->body;
         }
 
-        if (is_string($this->subtitle)) {
-            $alert[self::ALERT_TITLE_KEY] = $this->subtitle;
-        }
-
         if (is_string($this->titleLocKey)) {
             $alert[self::ALERT_TITLE_LOC_KEY] = $this->titleLocKey;
         }
